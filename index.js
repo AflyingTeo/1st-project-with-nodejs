12ad9afcb46a2ba93abadc8843d65e569df886d1
Teo Bay Bong <phamanh9296@icloud.com>
2021-05-12 18:55:07 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+//Midlleware bodyparser
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//Use template enegines Pug
+app.set('view engine', 'pug');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// })
+// app.get('/', function (req, res) {
+//     res.render('index', { title: 'Hey', message: 'Hello there!' })
+// }
+
+app.get('/', (req, res) => res.render('index',{ title: 'Home', message: 'Hello!!!!'}));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
